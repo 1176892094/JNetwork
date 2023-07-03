722e3c609695347c12281b368674d7620fdaeb2b
Charlotte <1176892094@qq.com>
2023-07-03 23:16:40 +0800

    增加Client和Server的更新

---
+++ Assets/JFrameworkNet/Runtime/Manager/NetworkLoop.cs
@@ -0,0 +1,67 @@
+using System;
+using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.PlayerLoop;
+using UpdateFunction = UnityEngine.LowLevel.PlayerLoopSystem.UpdateFunction;
+
+namespace JFramework.Net
+{
+    internal static class NetworkLoop
+    {
+        private static bool AddPlayerLoop(UpdateFunction function, ref PlayerLoopSystem playerLoop, Type systemType)
+        {
+            if (playerLoop.type == systemType)
+            {
+                if (Array.FindIndex(playerLoop.subSystemList, system => system.updateDelegate == function) != -1)
+                {
+                    return true;
+                }
+
+                int oldLength = playerLoop.subSystemList?.Length ?? 0;
+                Array.Resize(ref playerLoop.subSystemList, oldLength + 1);
+                playerLoop.subSystemList[oldLength] = new PlayerLoopSystem
+                {
+                    type = typeof(NetworkLoop),
+                    updateDelegate = function
+                };
+                return true;
+            }
+
+            if (playerLoop.subSystemList != null)
+            {
+                for (int i = 0; i < playerLoop.subSystemList.Length; ++i)
+                {
+                    if (AddPlayerLoop(function, ref playerLoop.subSystemList[i], systemType))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static void NetworkEarlyUpdate()
+        {
+            if (!Application.isPlaying) return;
+            NetworkServer.EarlyUpdate();
+            NetworkClient.EarlyUpdate();
+        }
+
+        private static void NetworkLateUpdate()
+        {
+            if (!Application.isPlaying) return;
+            NetworkServer.AfterUpdate();
+            NetworkClient.AfterUpdate();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void RuntimeInitializeOnLoad()
+        {
+            var playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+            AddPlayerLoop(NetworkEarlyUpdate, ref playerLoop, typeof(EarlyUpdate));
+            AddPlayerLoop(NetworkLateUpdate, ref playerLoop, typeof(PreLateUpdate));
+            PlayerLoop.SetPlayerLoop(playerLoop);
+        }
+    }
+}
