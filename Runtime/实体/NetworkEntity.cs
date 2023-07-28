da20b6fe9f5840b76aa0883606c23bb8c15fda18
Charlotte <1176892094@qq.com>
2023-07-29 05:41:38 +0800

    Update NetworkEntity.cs

--- Runtime/实体/NetworkEntity.cs
+++ Runtime/实体/NetworkEntity.cs
@@ -14,7 +14,7 @@ namespace JFramework.Net
         /// <summary>
         /// 实体Id
         /// </summary>
-        public int Id { get; private set; }
+        public int entityId { get; private set; }
 
         /// <summary>
         /// 实体更新
@@ -39,10 +39,10 @@ namespace JFramework.Net
         /// <summary>
         /// 实体Id
         /// </summary>
-        int IEntity.Id
+        int IEntity.entityId
         {
-            get => Id;
-            set => Id = value;
+            get => entityId;
+            set => entityId = value;
         }
 
         /// <summary>
