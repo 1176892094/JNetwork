5db0c6c1b1e9423bab9ad5d34f06c675161d9220
Charlotte <1176892094@qq.com>
2023-08-02 20:35:06 +0800

    Update NetworkEntity.cs

--- Runtime/实体/NetworkEntity.cs
+++ Runtime/实体/NetworkEntity.cs
@@ -1,6 +1,5 @@
 using JFramework.Core;
 using JFramework.Interface;
-using UnityEngine;
 
 namespace JFramework.Net
 {
@@ -10,11 +9,6 @@ namespace JFramework.Net
     public abstract class NetworkEntity : NetworkBehaviour, IEntity
     {
         /// <summary>
-        /// 实体Id
-        /// </summary>
-        public int entityId { get; private set; }
-
-        /// <summary>
         /// 实体更新
         /// </summary>`
         protected virtual void OnUpdate() { }
@@ -22,7 +16,7 @@ namespace JFramework.Net
         /// <summary>
         /// 实体启用
         /// </summary>
-        protected virtual void OnEnable() => GlobalManager.Listen(this);
+        protected virtual void OnEnable() => GlobalManager.Listen(this, gameObject);
 
         /// <summary>
         /// 实体禁用
@@ -33,25 +27,6 @@ namespace JFramework.Net
         /// 实体销毁 (如果能获取到角色接口 则销毁角色的控制器)
         /// </summary>
         protected virtual void OnDestroy() => GetComponent<ICharacter>()?.Destroy();
-
-        /// <summary>
-        /// 实体Id
-        /// </summary>
-        int IEntity.entityId
-        {
-            get => entityId;
-            set => entityId = value;
-        }
-
-        /// <summary>
-        /// 实体Transform
-        /// </summary>
-        Transform IEntity.transform => transform;
-
-        /// <summary>
-        /// 实体GameObject
-        /// </summary>
-        GameObject IEntity.gameObject => gameObject;
 
         /// <summary>
         /// 实体接口调用实体更新方法
