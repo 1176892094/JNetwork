db7a380290a66ad776a9071fc34a5cefa935f144
Charlotte <1176892094@qq.com>
2024-02-25 18:44:34 +0800

    Update NetworkAnimator.cs

--- Runtime/Component/NetworkAnimator.cs
+++ Runtime/Component/NetworkAnimator.cs
@@ -88,7 +88,7 @@ namespace JFramework.Net
         /// <summary>
         /// 初始化动画控制器
         /// </summary>
-        private void Awake()
+        protected override void Awake()
         {
             animatorParams = animator.parameters.Where(parameter => !animator.IsParameterControlledByCurve(parameter.nameHash)).ToArray();
             lastIntParams = new int[animatorParams.Length];
